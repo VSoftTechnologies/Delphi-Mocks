321ac518dee30f874d025c50e55a3f3b2243f738
Jason Smith <jason.nevar.smith@gmail.com>
2013-05-02 17:16:15 +1000

    Updated behavior and expectation tests
    Behavior and expectations tests have been updated along with an update
    to CreateNever to set the correct expectation type.

--- Tests/Delphi.Mocks.Tests.Behavior.pas
+++ Tests/Delphi.Mocks.Tests.Behavior.pas
@@ -24,10 +24,24 @@ type
     procedure Test_WillExecute;
 
     // WillRaise-Execute tests
-    procedure CreateWillRaise_Execute__Raises_Exception_Of_Our_Choice;
-    procedure CreateWillRaise_Execute__Raises_Exception_Message_Of_Our_Choice;
-    procedure CreateWillRaise_Execute__Raises_No_Exception_If_Passed_Nil_For_Exception_Class;
-    procedure CreateWillRaise_Execute__Raises_Exception_Of_Our_Choice_With_Default_Message;
+    procedure CreateWillRaise_Execute_Raises_Exception_Of_Our_Choice;
+    procedure CreateWillRaise_Execute_Raises_Exception_Message_Of_Our_Choice;
+    procedure CreateWillRaise_Execute_Raises_No_Exception_If_Passed_Nil_For_Exception_Class;
+    procedure CreateWillRaise_Execute_Raises_Exception_Of_Our_Choice_With_Default_Message;
+
+    // WillRaiseWhen-Exexute tests
+    procedure CreateWillRaiseWhen_Execute_Raises_Exception_Of_Our_Choice;
+    procedure CreateWillRaiseWhen_Execute_Raises_Exception_Message_Of_Our_Choice;
+    procedure CreateWillRaiseWhen_Execute_Raises_No_Exception_If_Passed_Nil_For_Exception_Class;
+    procedure CreateWillRaiseWhen_Execute_Raises_Exception_Of_Our_Choice_With_Default_Message;
+
+    // Test Behavior Types After Construction.
+    procedure CreateWillExecute_Behavior_Type_Set_To_WillExecute;
+    procedure CreateWillExecuteWhen_Behavior_Type_Set_To_WillExecuteWhen;
+    procedure CreateWillReturnWhen_Behavior_Type_Set_To_WillReturn;
+    procedure CreateReturnDefault_Behavior_Type_Set_To_ReturnDefault;
+    procedure CreateWillRaise_Behavior_Type_Set_To_WillAlwaysRaise;
+    procedure CreateWillRaiseWhen_Behavior_Type_Set_To_WillRaise;
   end;
 
 implementation
@@ -50,14 +64,8 @@ end;
 procedure TTestBehaviors.Test_WillExecute;
 var
   behavior : IBehavior;
-  returnValue : TValue;
-  args : TArray<TValue>;
-  rType : TRttiType;
+  returnValue: TValue;
 begin
-  SetLength(args,1);
-  args[0] := 999;
-  rType := FContext.GetType(TypeInfo(string));
-
   behavior := TBehavior.CreateWillExecute(
     function (const args : TArray<TValue>; const returnType : TRttiType) : TValue
     begin
@@ -65,7 +73,7 @@ begin
     end
     );
 
-  returnValue := behavior.Execute(args,rType);
+  returnValue := behavior.Execute(nil,nil);
 
   CheckTrue(SameText(returnValue.AsString,'hello world'));
 end;
@@ -122,7 +130,108 @@ begin
   CheckFalse(behavior.Match(args));
 end;
 
-procedure TTestBehaviors.CreateWillRaise_Execute__Raises_Exception_Message_Of_Our_Choice;
+procedure TTestBehaviors.CreateReturnDefault_Behavior_Type_Set_To_ReturnDefault;
+var
+  behavior: IBehavior;
+begin
+  behavior := TBehavior.CreateReturnDefault(nil);
+
+  Check(behavior.BehaviorType = TBehaviorType.ReturnDefault, 'CreateReturnDefault behavior type isn''t ReturnDefault');
+end;
+
+procedure TTestBehaviors.CreateWillExecuteWhen_Behavior_Type_Set_To_WillExecuteWhen;
+var
+  behavior: IBehavior;
+begin
+  behavior := TBehavior.CreateWillExecuteWhen(nil, nil);
+
+  Check(behavior.BehaviorType = TBehaviorType.WillExecuteWhen, 'CreateWillExecuteWhen behavior type isn''t WillExecuteWhen');
+end;
+
+procedure TTestBehaviors.CreateWillExecute_Behavior_Type_Set_To_WillExecute;
+var
+  behavior: IBehavior;
+begin
+  behavior := TBehavior.CreateWillExecute(nil);
+
+  Check(behavior.BehaviorType = TBehaviorType.WillExecute, 'CreateWillExecute behavior type isn''t WillExecute');
+end;
+
+procedure TTestBehaviors.CreateWillRaiseWhen_Behavior_Type_Set_To_WillRaise;
+var
+  behavior: IBehavior;
+begin
+  //What is passed here shouldn't affect the result of the behavior being set. No way to avoid it however.
+  behavior := TBehavior.CreateWillRaiseWhen(nil, ETestBehaviourException, '');
+
+  Check(behavior.BehaviorType = TBehaviorType.WillRaise, 'CreateWillRaiseWhen behavior type isn''t WillRaise');
+end;
+
+procedure TTestBehaviors.CreateWillRaiseWhen_Execute_Raises_Exception_Message_Of_Our_Choice;
+var
+  behavior: IBehavior;
+const
+  EXCEPTION_STRING = 'Exception!';
+begin
+  behavior := TBehavior.CreateWillRaiseWhen(nil, ETestBehaviourException, EXCEPTION_STRING);
+
+  StartExpectingException(ETestBehaviourException);
+
+  //Passing nils as we don't care about these values for the exception
+  behavior.Execute(nil, nil);
+
+  StopExpectingException('');
+end;
+
+procedure TTestBehaviors.CreateWillRaiseWhen_Execute_Raises_Exception_Of_Our_Choice;
+var
+  behavior: IBehavior;
+begin
+  behavior := TBehavior.CreateWillRaiseWhen(nil, ETestBehaviourException, '');
+
+  StartExpectingException(ETestBehaviourException);
+
+  //Passing nils as we don't care about these values for the exception
+  behavior.Execute(nil, nil);
+
+  StopExpectingException;
+end;
+
+procedure TTestBehaviors.CreateWillRaiseWhen_Execute_Raises_Exception_Of_Our_Choice_With_Default_Message;
+var
+  behavior: IBehavior;
+const
+  EXCEPTION_STRING = 'raised by mock';
+begin
+  behavior := TBehavior.CreateWillRaiseWhen(nil, ETestBehaviourException, '');
+
+  StartExpectingException(ETestBehaviourException);
+
+  //Passing nils as we don't care about these values for the exception
+  behavior.Execute(nil, nil);
+
+  StopExpectingException(EXCEPTION_STRING);
+end;
+
+procedure TTestBehaviors.CreateWillRaiseWhen_Execute_Raises_No_Exception_If_Passed_Nil_For_Exception_Class;
+var
+  behavior: IBehavior;
+begin
+  behavior := TBehavior.CreateWillRaise(nil, '');
+
+  //No exception coverage. Therefore we shouldn't get an exception
+  behavior.Execute(nil, nil);
+
+  //If we have gotten here no exception was recieved.
+  Check(True);
+end;
+
+procedure TTestBehaviors.CreateWillRaise_Behavior_Type_Set_To_WillAlwaysRaise;
+begin
+
+end;
+
+procedure TTestBehaviors.CreateWillRaise_Execute_Raises_Exception_Message_Of_Our_Choice;
 var
   behavior: IBehavior;
 const
@@ -138,7 +247,7 @@ begin
   StopExpectingException('');
 end;
 
-procedure TTestBehaviors.CreateWillRaise_Execute__Raises_Exception_Of_Our_Choice;
+procedure TTestBehaviors.CreateWillRaise_Execute_Raises_Exception_Of_Our_Choice;
 var
   behavior: IBehavior;
 begin
@@ -146,13 +255,14 @@ begin
 
   StartExpectingException(ETestBehaviourException);
 
+  //Passing nils as we don't care about these values for the exception
   behavior.Execute(nil, nil);
 
   StopExpectingException;
 end;
 
 
-procedure TTestBehaviors.CreateWillRaise_Execute__Raises_Exception_Of_Our_Choice_With_Default_Message;
+procedure TTestBehaviors.CreateWillRaise_Execute_Raises_Exception_Of_Our_Choice_With_Default_Message;
 var
   behavior: IBehavior;
 const
@@ -168,7 +278,7 @@ begin
   StopExpectingException(EXCEPTION_STRING);
 end;
 
-procedure TTestBehaviors.CreateWillRaise_Execute__Raises_No_Exception_If_Passed_Nil_For_Exception_Class;
+procedure TTestBehaviors.CreateWillRaise_Execute_Raises_No_Exception_If_Passed_Nil_For_Exception_Class;
 var
   behavior: IBehavior;
 begin
@@ -179,6 +289,11 @@ begin
 
   //If we have gotten here no exception was recieved.
   Check(True);
+end;
+
+procedure TTestBehaviors.CreateWillReturnWhen_Behavior_Type_Set_To_WillReturn;
+begin
+
 end;
 
 initialization
