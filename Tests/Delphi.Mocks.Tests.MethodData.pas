b7f87a6878084971788b3ec4a710c51c3d3fb408
Fabricio Colombo <fabricio.colombo.mva@gmail.com>
2015-05-16 16:49:45 -0300

    show the arguments to an unexpected method call with no default retun value defined. When calling a expected function with invalid arguments, the expectation is not matched, but the exception does not show the args used.

--- Tests/Delphi.Mocks.Tests.MethodData.pas
+++ Tests/Delphi.Mocks.Tests.MethodData.pas
@@ -10,6 +10,12 @@ uses
   Delphi.Mocks.Interfaces;
 
 type
+  {$M+}
+  ISimpleInterface = interface
+    function MissingArg(Value: Integer): Integer;
+  end;
+  {$M-}
+
   TTestMethodData = class(TTestCase)
   published
     procedure Expectation_Before_Verifies_To_True_When_Prior_Method_Called_Atleast_Once;
@@ -21,6 +27,8 @@ type
 
     procedure BehaviourMustBeDefined_IsFalse_AndBehaviourIsNotDefined_RaisesNoException;
     procedure BehaviourMustBeDefined_IsTrue_AndBehaviourIsNotDefined_RaisesException;
+
+    procedure RaiseExceptionWhenMissingArgsAndDontHaveDefaultValue;
   end;
 
 implementation
@@ -28,7 +36,7 @@ implementation
 uses
   Delphi.Mocks.Helpers,
   Delphi.Mocks.MethodData,
-  classes, System.TypInfo;
+  classes, System.TypInfo, StrUtils;
 
 
 { TTestMethodData }
@@ -36,6 +44,28 @@ uses
 procedure TTestMethodData.Expectation_Before_Verifies_To_True_When_Prior_Method_Called_Atleast_Once;
 begin
   Check(False, 'Not implemented');
+end;
+
+procedure TTestMethodData.RaiseExceptionWhenMissingArgsAndDontHaveDefaultValue;
+const
+  EXPECTED_ERROR_MESSAGE = 'method [MissingArg] with args ( 4 )';
+var
+  vMock: TMock<ISimpleInterface>;
+begin
+  vMock := TMock<ISimpleInterface>.Create;
+  vMock.Setup.Expect.Once.When.MissingArg(123);
+
+  try
+    vMock.Instance.MissingArg(4);
+    Fail('Do not raise Exception');
+  except
+    on E: Exception do
+    begin
+      CheckEquals(EMockException.ClassName, E.ClassName, 'Expect a MockException');
+      CheckTrue(ContainsText(E.Message, EXPECTED_ERROR_MESSAGE),
+                Format('Expect the string <%s> contains the substring <%s>',[E.Message, EXPECTED_ERROR_MESSAGE]));
+    end;
+  end;
 end;
 
 procedure TTestMethodData.AllowRedefineBehaviorDefinitions_IsTrue_RedefinedIsAllowed;
